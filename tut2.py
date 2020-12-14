d5aca5336ffc6a6f4d5a296fa94bb0c19099eb85
Ayaan <75617112+Technogenair-Ayaan@users.noreply.github.com>
2020-12-14 17:50:03 +0530

    Image
    Tutorial 1 was deleted due to some reason so I am sharing tut2 project which is very important as it includes variables, inputs and more.

---
+++ tut2.py
@@ -0,0 +1,16 @@
+box = "hello"
+box1 = 12
+box2 = 12.2
+print(type(box))
+
+print(type(box2))
+
+var = float(box1)
+
+print(type(var))
+
+print("Enter number")
+num = int(input())
+auto = 10
+sum = num + auto
+print("The output is:",sum)
