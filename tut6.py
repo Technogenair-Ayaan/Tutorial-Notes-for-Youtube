0ddbc0863382597e9647f8590f097daf9a7433c0
Ayaan <75617112+Technogenair-Ayaan@users.noreply.github.com>
2020-12-31 17:51:47 +0530

    Python File
    This file contains all the notes of python tutorial series #6 video.

---
+++ tut6.py
@@ -0,0 +1,13 @@
+#Endswith
+mystr = "Hello I am Ayaan"
+print(mystr.endswith("Ayaan")) #-Output will be boolean
+#Count
+print(mystr.count("l")) #Printed the no. of the letters in my string
+#Find
+print(mystr.find("am")) #It tells you the position of the word in your string
+#lower
+my = "CAPITAL"
+print(my.lower()) #It convert the piece of string into lower case
+#upper
+st = "money"
+print(st.upper()) #It convert the piece of string into upper case
